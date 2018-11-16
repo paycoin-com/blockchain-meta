373cb300fb030993f51033404407990b79e6a438
nisakov <nurkali@gmail.com>
2018-11-16 17:05:48 +0600

    Fix, IPFS truncate data before writing files to.

--- bex-datastore/src/main/java/io/hs/bex/datastore/handler/ipfs/IpfsHandler.java
+++ bex-datastore/src/main/java/io/hs/bex/datastore/handler/ipfs/IpfsHandler.java
@@ -188,7 +188,7 @@ public class IpfsHandler implements DataStoreHandler
             // ---------------------------------
 
             path = rootDir + "/" + path + "/" + fileName;
-            url += "?arg=" + path + "&create=true";
+            url += "?arg=" + path + "&truncate=true&create=true";
 
             MultiValueMap<String, Object> bodyMap = new LinkedMultiValueMap<>();
             bodyMap.add( "user-file", data.getBytes() );
